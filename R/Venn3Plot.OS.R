2d7d11815bfd342cb4b187a42ae1a11357a2a675
Magdalena Arnal <mgdrnl@gmail.com>
2019-07-09 11:57:18 +0200

    Update Venn3Plot.OS.R

--- R/Venn3Plot.OS.R
+++ R/Venn3Plot.OS.R
@@ -1,6 +1,6 @@
 Venn3Plot.OS <-
 function(listG1, listG2, listG3, listNames, filename, data4T= NULL,
-                      mkExcel = TRUE, img.fmt = "png"){
+                      mkExcel = TRUE, img.fmt = "pdf"){
   #Funció per fer un venn diagram 3D
   #listG1: Vector of genes in group1
   #listG2: Vector of genes in group2
