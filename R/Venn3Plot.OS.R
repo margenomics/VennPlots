c38a09772b1301be93393d81a50868271410076b
jperera-bel <jperera@imim.es>
2019-10-10 12:15:29 +0200

    Venn3Plot.OS.R now uses openxlsx

--- R/Venn3Plot.OS.R
+++ R/Venn3Plot.OS.R
@@ -9,10 +9,12 @@ function(listG1, listG2, listG3, listNames, filename, data4T= NULL,
   #filename: nom dels fitxers
   #data4T: Data4Tyers
   #mkExcel: if TRUE an excel is made with the results of the venn diagram
+  #11/10/19 use 'openxlsx' package to write xlsx files, no java dependencies
   
   require(Vennerable) 
   require(colorfulVennPlot) #Per generar plots amb colors diferents
   require(RColorBrewer)  
+  require(openxlsx)
   #establim els colors per als plots
   cols <- brewer.pal(8,"Pastel2") 
   
@@ -39,41 +41,41 @@ function(listG1, listG2, listG3, listNames, filename, data4T= NULL,
   
   if(mkExcel){
     #EXCEL
-    options( java.parameters = "-Xmx4g" ) #super important abans de cridar a XLConnect
-    require(XLConnect)
-    xlcFreeMemory()
     #cNames <- colnames(data4T)[-grep("scaled$", colnames(data4T))]
     cNames <- colnames(data4T)
-      wb <- loadWorkbook(file.path(resultsDir,paste("GeneLists",filename,"xlsx",sep=".")),create=TRUE) #no li agraden els espais al nom o noms llargs!
-      createSheet(wb, name = "orange") 
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`100`)),
+    hs1 <- createStyle(fgFill = "#737373", halign = "CENTER", textDecoration = "Bold",
+                       border = "Bottom", fontColour = "white")
+    
+      wb <- createWorkbook()
+      addWorksheet(wb, sheetName = "orange") 
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`100`)),
                                cNames], 
-                     sheet = "orange", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "blue")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`010`)),
+                     sheet = "orange", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "blue")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`010`)),
                                cNames], 
-                     sheet = "blue", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "green")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`001`)),
+                     sheet = "blue", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "green")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`001`)),
                                cNames], 
-                     sheet = "green", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "pink")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`110`)),
+                     sheet = "green", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "pink")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`110`)),
                                cNames], 
-                     sheet = "pink", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "brown")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`011`)),
+                     sheet = "pink", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "brown")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`011`)),
                                cNames], 
-                     sheet = "brown", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "yellow")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`101`)),
+                     sheet = "brown", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "yellow")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`101`)),
                                cNames], 
-                     sheet = "yellow", startRow = 1, startCol = 1, header=TRUE)
-      createSheet(wb, name = "Common grey")
-      writeWorksheet(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`111`)),
+                     sheet = "yellow", startRow = 1, startCol = 1, headerStyle = hs1)
+      addWorksheet(wb, sheetName = "Common grey")
+      writeData(wb,data4T[!is.na(data4T[,"Symbol"]) & (data4T[,"Symbol"] %in% unlist(vtest@IntersectionSets$`111`)),
                                cNames], 
-                     sheet = "Common grey", startRow = 1, startCol = 1, header=TRUE)
-      saveWorkbook(wb)
+                     sheet = "Common grey", startRow = 1, startCol = 1, headerStyle = hs1)
+      saveWorkbook(wb,file=file.path(resultsDir,paste("GeneLists",filename,"xlsx",sep=".")),overwrite = TRUE)
       
   }
 }
